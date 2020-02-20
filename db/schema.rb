b4e33149e233c85816391a6e5c51b9fe56a940a9
ruorinshin960 <ruorin.sin960za@gmail.com>
2020-02-20 15:25:24 +0900

    user function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_121418) do
+ActiveRecord::Schema.define(version: 2020_02_19_171555) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 2020_02_15_121418) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.text "uid"
+    t.string "oauth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
