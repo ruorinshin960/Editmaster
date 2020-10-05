c0eb21734665a7a528a8e2d3b650d983f4651d05
mkt-wakabayashi <54176207+mkt-wakabayashi@users.noreply.github.com>
2020-10-06 05:28:47 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.12.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'Editmaster'
-set :deploy_to, '/var/www/Editmaster'
+
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:ruorinshin960/Editmaster.git'
 
