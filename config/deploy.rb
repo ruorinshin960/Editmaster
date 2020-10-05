126b23d8376a8257b32f2e0249669bc59ddafd5f
mkt-wakabayashi <54176207+mkt-wakabayashi@users.noreply.github.com>
2020-10-06 05:14:27 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.12.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'Editmaster'
-
+set :deploy_to, '/var/www/Editmaster'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:ruorinshin960/Editmaster.git'
 
@@ -31,16 +31,4 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'upload secrets.yml'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
